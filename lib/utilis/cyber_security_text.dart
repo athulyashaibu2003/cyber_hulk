2076f477dbb575d9a09c611fc2ab0618acc10a29
athulyashaibu2003 <athulyashaibu2003@gmail.com>
2024-09-12 11:32:12 +0530

    updated screens

--- lib/utilis/cyber_security_text.dart
+++ lib/utilis/cyber_security_text.dart
@@ -4,7 +4,6 @@ import 'dart:ffi';
 
 import 'package:cyber_hulk/utilis/container_text.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class CyberSecuritytext extends StatefulWidget {
   const CyberSecuritytext({super.key});
